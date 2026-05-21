af3246395e42ac1eb665117375a6ecd59e7995b7
Szymon Brodawka <szymbrod@gmail.com>
2026-05-21 16:08:50 +0200

    Add scripts reseting tables from the db and removing tables from the db

---
+++ db/scripts/remove_db.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS fishing_methods_fish CASCADE;
+DROP TABLE IF EXISTS fishing_methods_en_translations CASCADE;
+DROP TABLE IF EXISTS fishing_methods_pl_translations CASCADE;
+DROP TABLE IF EXISTS fishing_methods CASCADE;
+
+DROP TABLE IF EXISTS recipes_ingredients CASCADE;
+DROP TABLE IF EXISTS ingredients_en_translations CASCADE;
+DROP TABLE IF EXISTS ingredients_pl_translations CASCADE;
+DROP TABLE IF EXISTS ingredients CASCADE;
+
+DROP TABLE IF EXISTS recipes_fish CASCADE;
+DROP TABLE IF EXISTS recipes_en_translations CASCADE;
+DROP TABLE IF EXISTS recipes_pl_translations CASCADE;
+DROP TABLE IF EXISTS recipes CASCADE;
+
+DROP TABLE IF EXISTS fish_en_translations CASCADE;
+DROP TABLE IF EXISTS fish_pl_translations CASCADE;
+DROP TABLE IF EXISTS fish CASCADE;
+
+DROP TABLE IF EXISTS fishing_areas_en_translations CASCADE;
+DROP TABLE IF EXISTS fishing_areas_pl_translations CASCADE;
+DROP TABLE IF EXISTS fishing_areas CASCADE;
+
+DROP TABLE IF EXISTS user_records CASCADE;
+
+DROP TABLE IF EXISTS users_roles CASCADE;
+DROP TABLE IF EXISTS roles CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
+DROP TABLE IF EXISTS with_url_education_materials CASCADE;
+DROP TABLE IF EXISTS equipment CASCADE;
+DROP TABLE IF EXISTS education_materials CASCADE;
+
+DROP TABLE IF EXISTS alembic_version;
